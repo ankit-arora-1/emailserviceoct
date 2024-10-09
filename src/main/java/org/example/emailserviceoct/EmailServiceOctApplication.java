e81b88fd7b7f5244a84be8f4703ddf226749a6e6
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-10-09 09:29:39 +0530

    Added email service

---
+++ src/main/java/org/example/emailserviceoct/EmailServiceOctApplication.java
@@ -0,0 +1,13 @@
+package org.example.emailserviceoct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceOctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceOctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailserviceoct/EmailServiceOctApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailserviceoct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceOctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
